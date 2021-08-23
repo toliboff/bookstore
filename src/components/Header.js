5f4f6b1ed7c9ecf1a4b5e504afa4fd405dae0f06
Tolibjon <tolibjohn@gmail.com>
2021-08-23 18:50:27 +0500

    Add Books component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div>
     <h1>Bookstore CMS</h1>
 
     <ul>
-      <li><NavLink exact to="/">Books</NavLink></li>
-      <li><NavLink to="/categories">Categories</NavLink></li>
+      <li><Link exact to="/">Books</Link></li>
+      <li><Link to="/categories">Categories</Link></li>
     </ul>
 
     <div>
