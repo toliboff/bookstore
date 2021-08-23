f5e09185015c0b7da513866e51c29424b82d7979
Tolibjon <tolibjohn@gmail.com>
2021-08-23 15:21:09 +0500

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Books from './components/Books';
+import Categories from './components/Categories';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Books />
+        </Route>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
+
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
